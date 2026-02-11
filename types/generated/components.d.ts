77f2cde05c3812bb4cf918f62bb305f727b090a4
ArchieAtBeryl8 <archawudh.e@beryl8.com>
2026-02-11 09:51:17 +0700

    Ignore Strapi data folder

---
+++ types/generated/components.d.ts
@@ -0,0 +1,152 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface SectionsCustomersSection extends Struct.ComponentSchema {
+  collectionName: 'components_sections_customers_sections';
+  info: {
+    displayName: 'customers-section';
+  };
+  attributes: {
+    logos: Schema.Attribute.Component<'shared.customer-logo', true>;
+    title_highlight: Schema.Attribute.String;
+    title_normal: Schema.Attribute.String;
+  };
+}
+
+export interface SectionsHero extends Struct.ComponentSchema {
+  collectionName: 'components_sections_heroes';
+  info: {
+    displayName: 'hero';
+  };
+  attributes: {
+    background_image: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    cta_link: Schema.Attribute.String;
+    cta_text: Schema.Attribute.String;
+    description: Schema.Attribute.Blocks;
+    headline_line_1: Schema.Attribute.String;
+    headline_line_2: Schema.Attribute.String;
+  };
+}
+
+export interface SectionsServicesSection extends Struct.ComponentSchema {
+  collectionName: 'components_sections_services_sections';
+  info: {
+    displayName: 'services-section';
+  };
+  attributes: {
+    image: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
+    services: Schema.Attribute.Component<'shared.service-item', true>;
+    title_highlight: Schema.Attribute.String;
+    title_normal: Schema.Attribute.String;
+  };
+}
+
+export interface SharedCustomerLogo extends Struct.ComponentSchema {
+  collectionName: 'components_shared_customer_logos';
+  info: {
+    displayName: 'customer-logo';
+  };
+  attributes: {
+    logo: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
+    name: Schema.Attribute.String;
+    url: Schema.Attribute.String;
+  };
+}
+
+export interface SharedMedia extends Struct.ComponentSchema {
+  collectionName: 'components_shared_media';
+  info: {
+    displayName: 'Media';
+    icon: 'file-video';
+  };
+  attributes: {
+    file: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
+  };
+}
+
+export interface SharedQuote extends Struct.ComponentSchema {
+  collectionName: 'components_shared_quotes';
+  info: {
+    displayName: 'Quote';
+    icon: 'indent';
+  };
+  attributes: {
+    body: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SharedRichText extends Struct.ComponentSchema {
+  collectionName: 'components_shared_rich_texts';
+  info: {
+    description: '';
+    displayName: 'Rich text';
+    icon: 'align-justify';
+  };
+  attributes: {
+    body: Schema.Attribute.RichText;
+  };
+}
+
+export interface SharedSeo extends Struct.ComponentSchema {
+  collectionName: 'components_shared_seos';
+  info: {
+    description: '';
+    displayName: 'Seo';
+    icon: 'allergies';
+    name: 'Seo';
+  };
+  attributes: {
+    metaDescription: Schema.Attribute.Text & Schema.Attribute.Required;
+    metaTitle: Schema.Attribute.String & Schema.Attribute.Required;
+    shareImage: Schema.Attribute.Media<'images'>;
+  };
+}
+
+export interface SharedServiceItem extends Struct.ComponentSchema {
+  collectionName: 'components_shared_service_items';
+  info: {
+    displayName: 'service-item';
+  };
+  attributes: {
+    description: Schema.Attribute.Blocks;
+    icon: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SharedSlider extends Struct.ComponentSchema {
+  collectionName: 'components_shared_sliders';
+  info: {
+    description: '';
+    displayName: 'Slider';
+    icon: 'address-book';
+  };
+  attributes: {
+    files: Schema.Attribute.Media<'images', true>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'sections.customers-section': SectionsCustomersSection;
+      'sections.hero': SectionsHero;
+      'sections.services-section': SectionsServicesSection;
+      'shared.customer-logo': SharedCustomerLogo;
+      'shared.media': SharedMedia;
+      'shared.quote': SharedQuote;
+      'shared.rich-text': SharedRichText;
+      'shared.seo': SharedSeo;
+      'shared.service-item': SharedServiceItem;
+      'shared.slider': SharedSlider;
+    }
+  }
+}
